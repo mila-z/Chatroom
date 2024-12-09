ccd2f5b0ed3413fcdf06940a6b6aa6933fc71bc9
mila-z <m_zaprianova@protonmail.com>
2024-12-09 12:45:01 +0200

    added a function to the server that sends connected and logged out notifications

--- server.py
+++ server.py
@@ -12,6 +12,8 @@ server_socket.bind((IP, PORT))
 
 server_socket.listen()
 
+print('Server is listening...')
+
 # the list of clients = the list of sockets
 sockets_list = [server_socket]
 
@@ -51,21 +53,42 @@ while True:
             clients[client_socket] = user
 
             print(f'Accepted new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}')
+
+            connection_message = f'{user['data'].decode('utf-8')} has joined the chat!'.encode('utf-8')
+            connection_message_header = f'{len(connection_message):<{HEADER_LENGTH}}'.encode('utf-8')
+            for client in clients:
+                if client != client_socket:
+                    client.send(connection_message_header + connection_message)
+            
+            confirmation_message = 'You have joined the chat!'.encode('utf-8')
+            confirmation_message_header = f'{len(confirmation_message):<{HEADER_LENGTH}}'.encode('utf-8')
+            client_socket.send(confirmation_message_header + confirmation_message)
+
         else:
             message = receive_message(notified_socket)
             if message is False:
-                print(f'Closed connection from {clients[notified_socket]['data'].decode('utf-8')}')
+                user_left = clients[notified_socket]['data'].decode('utf-8')
+                print(f'Closed connection from {user_left}')
                 sockets_list.remove(notified_socket)
                 del clients[notified_socket]
+                
+                # notify users for the leaving of user
+                leave_message = f'{user_left} has left the chat!'.encode('utf-8')
+                leave_message_header = f'{len(leave_message):<{HEADER_LENGTH}}'.encode('utf-8')
+                for client in clients:
+                    client.send(leave_message_header + leave_message)
+                
                 continue
             
             user = clients[notified_socket]
             print(f'Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}')
 
-            # move into a broadcast function
+            # move into a broadcast function?
             for client_socket in clients:
                 if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+                    mess = f'{user['data'].decode('utf-8')} > {message['data'].decode('utf-8')}'.encode('utf-8')
+                    mess_header = f'{len(mess):<{HEADER_LENGTH}}'.encode('utf-8')
+                    client_socket.send(mess_header + mess)
     
     for notified_socket in exception_sockets:
         sockets_list.remove(notified_socket)
