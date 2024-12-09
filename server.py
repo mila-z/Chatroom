bf7772aaf44471eae13042d6d273ab244a307045
mila-z <m_zaprianova@protonmail.com>
2024-12-09 14:14:33 +0200

    clean up code

--- server.py
+++ server.py
@@ -18,6 +18,11 @@ sockets_list = [server_socket]
 
 clients = {}
 
+
+def generate_header(message):
+    return f'{len(message):<{HEADER_LENGTH}}'.encode('utf-8')
+
+
 def receive_message(client_socket):
     try:
         message_header = client_socket.recv(HEADER_LENGTH)
@@ -29,9 +34,67 @@ def receive_message(client_socket):
         message_length = int(message_header.decode('utf-8').strip())
         return {'header': message_header, 'data': client_socket.recv(message_length)}
     #if someone closed abruptly
-    except: 
+    except Exception as e: 
+        print(f'Error receiving message: {e}')
         return False
 
+
+def send_message_to_all_except_sender(sender_socket, message):
+    for client_socket in clients:
+        if client_socket != sender_socket:
+            client_socket.send(message)
+
+
+# accepting and handling new connection
+def handle_new_connection(client_socket, client_address):
+    # adding information to the constants
+    sockets_list.append(client_socket)
+
+    clients[client_socket] = user
+
+    print(f'Accepted new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}')
+
+    # notifying everybody besides the new user, that the new user has joined the chat
+    connection_message = f'{user['data'].decode('utf-8')} has joined the chat!'.encode('utf-8')
+    connection_message_header = generate_header(connection_message)
+    send_message_to_all_except_sender(client_socket, connection_message_header + connection_message)
+            
+    # confirming to the new user that they have successfully joined the chatroom   
+    confirmation_message = 'You have joined the chat!'.encode('utf-8')
+    confirmation_message_header = generate_header(confirmation_message)
+    client_socket.send(confirmation_message_header + confirmation_message)
+
+
+# handling a disconnection
+def handle_disconnection(disconnected_socket):
+    disconnected_user = clients[disconnected_socket]['data'].decode('utf-8')
+
+    print(f'Closed connection from {disconnected_user}')
+    
+    sockets_list.remove(disconnected_socket)
+    del clients[disconnected_socket]
+                
+    # notify the rest of the users for the leaving of user
+    leave_message = f'{disconnected_user} has left the chat!'.encode('utf-8')
+    leave_message_header = generate_header(leave_message)
+
+    for client in clients:
+        client.send(leave_message_header + leave_message)
+
+
+# broadcast sent message
+def broadcast_message_to_users(sender_socket, message):
+    user = clients[sender_socket]
+
+    username = user['data'].decode('utf-8')
+    message_data = message['data'].decode('utf-8')
+
+    print(f'Received message from {username}: {message_data}')
+
+    broadcast_message = f'{username} > {message_data}'.encode('utf-8')
+    broadcast_message_header = generate_header(broadcast_message)
+
+    send_message_to_all_except_sender(sender_socket, broadcast_message_header + broadcast_message)
 
 
 while True:
@@ -39,7 +102,7 @@ while True:
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
     for notified_socket in read_sockets:
-        # if someone just connected, accepting and handling the new connection 
+        # new connection
         if notified_socket == server_socket: 
             client_socket, client_address = server_socket.accept()
 
@@ -48,52 +111,17 @@ while True:
             if user is False: 
                 continue
 
-            sockets_list.append(client_socket)
-
-            clients[client_socket] = user
-
-            print(f'Accepted new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}')
-
-            # notifying everybody besides the new user, that the new user has joined the chat
-            connection_message = f'{user['data'].decode('utf-8')} has joined the chat!'.encode('utf-8')
-            connection_message_header = f'{len(connection_message):<{HEADER_LENGTH}}'.encode('utf-8')
-            for client in clients:
-                if client != client_socket:
-                    client.send(connection_message_header + connection_message)
-            
-            # confirming to the new user that they have successfully joined the chatroom   
-            confirmation_message = 'You have joined the chat!'.encode('utf-8')
-            confirmation_message_header = f'{len(confirmation_message):<{HEADER_LENGTH}}'.encode('utf-8')
-            client_socket.send(confirmation_message_header + confirmation_message)
-
+            handle_new_connection(client_socket, client_address)
+        # standard message
         else:
             message = receive_message(notified_socket)
 
-            # handling a disconnected user
+            # lost connection
             if message is False:
-                user_left = clients[notified_socket]['data'].decode('utf-8')
-                print(f'Closed connection from {user_left}')
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                
-                # notify the rest of the users for the leaving of user
-                leave_message = f'{user_left} has left the chat!'.encode('utf-8')
-                leave_message_header = f'{len(leave_message):<{HEADER_LENGTH}}'.encode('utf-8')
-                for client in clients:
-                    client.send(leave_message_header + leave_message)
-                
+                handle_disconnection(notified_socket)
                 continue
             
-            user = clients[notified_socket]
-            
-            print(f'Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}')
-
-            # send the message to everybody besides the 'sender'
-            for client_socket in clients:
-                if client_socket != notified_socket:
-                    mess = f'{user['data'].decode('utf-8')} > {message['data'].decode('utf-8')}'.encode('utf-8')
-                    mess_header = f'{len(mess):<{HEADER_LENGTH}}'.encode('utf-8')
-                    client_socket.send(mess_header + mess)
+            broadcast_message_to_users(notified_socket, message)
     
     for notified_socket in exception_sockets:
         sockets_list.remove(notified_socket)
