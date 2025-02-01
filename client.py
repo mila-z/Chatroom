518d2f1551aed35f354e87e1aead6aa634f8a354
mila-z <m_zaprianova@protonmail.com>
2025-02-01 14:09:48 +0200

    encapsulate

--- client.py
+++ client.py
@@ -2,91 +2,84 @@ import socket
 import errno
 import threading
 
-HEADER_LENGTH = 10
+class ChatClient:
+    def __init__(self, ip, port, username):
+        self.HEADER_LENGTH = 10
+        self.IP = ip
+        self.PORT = port
+        self.username = username.encode('utf-8')
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.termination_flag = threading.Event()
 
-IP = '127.0.0.1'
-PORT = 1234
+    def connect(self):
+        self.client_socket.connect((self.IP, self.PORT))
+        self.client_socket.setblocking(False) 
 
-my_username = input('Username: ')
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False) 
+        # send the username to the server
+        self.username_header = self.generate_header(self.username)
+        self.client_socket.send(self.username_header + self.username)
 
-def generate_header(message):
-    return f'{len(message):<{HEADER_LENGTH}}'.encode('utf-8')
+    def generate_header(self, message):
+        return f'{len(message):<{self.HEADER_LENGTH}}'.encode('utf-8')
 
-# get the username 
-username = my_username.encode('utf-8')
-username_header = generate_header(username)
+    def receive_messages(self):
+        while not self.termination_flag.is_set():
+            try:
+                # receiving messages from the server 
+                while True:
+                    header = self.client_socket.recv(self.HEADER_LENGTH)
 
-# send the username to the server
-client_socket.send(username_header + username)
+                    if not len(self.username_header):
+                        print('Connection closed by the server')
+                        self.termination_flag.set()
+                        break
 
-termination_flag = threading.Event()
+                    message_len = int(header.decode('utf-8').strip())
+                    message = self.client_socket.recv(message_len).decode('utf-8')
 
-def receive_messages():
-    while not termination_flag.is_set():
-        try:
-            # receiving messages from the server 
-            while True:
-                header = client_socket.recv(HEADER_LENGTH)
-
-                if not len(username_header):
-                    print('Connection closed by the server')
-                    termination_flag.set()
+                    print(message)
+            except IOError as e:
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK: #when there are no more messages to be received
+                    print('Reading error', str(e))
+                    self.client_socket.close()
+                    self.termination_flag.set()
                     break
-
-                message_len = int(header.decode('utf-8').strip())
-                message = client_socket.recv(message_len).decode('utf-8')
-
-                print(message)
-        except IOError as e:
-            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK: #when there are no more messages to be received
-                print('Reading error', str(e))
-                
-                client_socket.close()
-                termination_flag.set()
-                
+            except Exception as e:
+                print('General error', str(e))
+                self.client_socket.close()
+                self.termination_flag.set()
                 break
-        except Exception as e:
-            print('General error', str(e))
 
-            client_socket.close()
-            termination_flag.set()
-            
-            break
+    def send_messages(self):
+        while not self.termination_flag.is_set():
+            message = input("")
 
+            if message == '!logout':
+                print('Logging out...')
+                self.termination_flag.set()
+                self.client_socket.close()
+                break
+            elif message:
+                message = message.encode('utf-8')
+                message_header = self.generate_header(message)
+                self.client_socket.send(message_header + message)
 
-def send_messages():
-    while not termination_flag.is_set():
-        message = input("")
+    def run(self):
+        self.connect()
 
-        if message == '!logout':
-            print('Logging out...')
+        # creating new threads that will handle receiving and sending messages from and to other users
+        self.receive_thread = threading.Thread(target=self.receive_messages, daemon=True)
+        self.send_thread = threading.Thread(target=self.send_messages, daemon=True)
 
-            termination_flag.set()
-            client_socket.close()
+        # start the threads
+        self.send_thread.start()
+        self.receive_thread.start()
 
-            break
-        elif message:
-            message = message.encode('utf-8')
-            message_header = generate_header(message)
+        self.send_thread.join()
+        self.termination_flag.set()
+        self.receive_thread.join()
 
-            client_socket.send(message_header + message)
-
-
-# creating new threads that will handle receiving and sending messages from and to other users
-receive_thread = threading.Thread(target=receive_messages, daemon=True)
-send_thread = threading.Thread(target=send_messages, daemon=True)
-
-
-# start the threads
-send_thread.start()
-receive_thread.start()
-
-
-send_thread.join()
-termination_flag.set()
-receive_thread.join()
-
-
+if __name__ == "__main__":
+    username = input('Username: ')
+    client = ChatClient('127.0.0.1', 1234, username)
+    client.run()
--- server.py
+++ server.py
@@ -1,169 +1,167 @@
 import socket
 import select 
 
-HEADER_LENGTH = 10
-IP = '127.0.0.1'
-PORT = 1234
+class ChatServer:
+    def __init__(self, ip, port):
+        self.HEADER_LENGTH = 10
+        self.IP = ip
+        self.PORT = port
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.IP, self.PORT))
+        self.server_socket.listen()
 
-server_socket.bind((IP, PORT))
+        print('Server is listening...')
 
-server_socket.listen()
-
-print('Server is listening...')
-
-sockets_list = [server_socket]
-
-# key: socket, value: {'header': username header, 'data': username}
-clients = {}
-
-
-def generate_header(message):
-    return f'{len(message):<{HEADER_LENGTH}}'.encode('utf-8')
+        self.sockets_list = [self.server_socket]
+        # key: socket, value: {'header': username header, 'data': username}
+        self.clients = {}
 
 
-def receive_message(client_socket):
-    try:
-        message_header = client_socket.recv(HEADER_LENGTH)
+    def generate_header(self, message):
+        return f'{len(message):<{self.HEADER_LENGTH}}'.encode('utf-8')
 
-        # if there is no data, the client closed the connection
-        if not len(message_header):
+
+    def receive_message(self, client_socket):
+        try:
+            message_header = client_socket.recv(self.HEADER_LENGTH)
+
+            # if there is no data, the client closed the connection
+            if not len(message_header):
+                return False
+
+            message_length = int(message_header.decode('utf-8').strip())
+            return {'header': message_header, 'data': client_socket.recv(message_length)}
+        #if someone closed abruptly
+        except Exception as e: 
+            print(f'Error receiving message: {e}')
             return False
 
-        message_length = int(message_header.decode('utf-8').strip())
-        return {'header': message_header, 'data': client_socket.recv(message_length)}
-    #if someone closed abruptly
-    except Exception as e: 
-        print(f'Error receiving message: {e}')
-        return False
+    def send_message_to_all_except_sender(self, sender_socket, message):
+        for client_socket in self.clients:
+            if client_socket != sender_socket:
+                client_socket.send(message)
 
 
-def send_message_to_all_except_sender(sender_socket, message):
-    for client_socket in clients:
-        if client_socket != sender_socket:
-            client_socket.send(message)
+    # accepting and handling new connection
+    def handle_new_connection(self, client_socket, client_address, user):
+        self.sockets_list.append(client_socket)
+        self.clients[client_socket] = user
+
+        print(f'Accepted new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}')
+
+        # notifying everybody besides the new user, that the new user has joined the chat
+        connection_message = f'{user['data'].decode('utf-8')} has joined the chat!'.encode('utf-8')
+        connection_message_header = self.generate_header(connection_message)
+        self.send_message_to_all_except_sender(client_socket, connection_message_header + connection_message)
+
+        # confirming to the new user that they have successfully joined the chatroom   
+        confirmation_message = 'You have joined the chat!'.encode('utf-8')
+        confirmation_message_header = self.generate_header(confirmation_message)
+        client_socket.send(confirmation_message_header + confirmation_message)
 
 
-# accepting and handling new connection
-def handle_new_connection(client_socket, client_address, user):
-    sockets_list.append(client_socket)
+    # handling a disconnection
+    def handle_disconnection(self, disconnected_socket):
+        disconnected_user = self.clients[disconnected_socket]['data'].decode('utf-8')
 
-    clients[client_socket] = user
+        print(f'Closed connection from {disconnected_user}')
 
-    print(f'Accepted new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}')
+        self.sockets_list.remove(disconnected_socket)
+        del self.clients[disconnected_socket]
 
-    # notifying everybody besides the new user, that the new user has joined the chat
-    connection_message = f'{user['data'].decode('utf-8')} has joined the chat!'.encode('utf-8')
-    connection_message_header = generate_header(connection_message)
-    send_message_to_all_except_sender(client_socket, connection_message_header + connection_message)
-            
-    # confirming to the new user that they have successfully joined the chatroom   
-    confirmation_message = 'You have joined the chat!'.encode('utf-8')
-    confirmation_message_header = generate_header(confirmation_message)
-    client_socket.send(confirmation_message_header + confirmation_message)
+        # notify the rest of the users for the leaving of user
+        leave_message = f'{disconnected_user} has left the chat!'.encode('utf-8')
+        leave_message_header = self.generate_header(leave_message)
+
+        for client in self.clients:
+            client.send(leave_message_header + leave_message)
 
 
-# handling a disconnection
-def handle_disconnection(disconnected_socket):
-    disconnected_user = clients[disconnected_socket]['data'].decode('utf-8')
+    def show_active_users(self, sender_socket):
+        for client_socket in self.clients:
+            if client_socket != sender_socket:
+                message = ('->' + self.clients[client_socket]['data'].decode('utf-8')).encode('utf-8')
+                message_header = self.generate_header(message)
+                sender_socket.send(message_header + message)
+            elif len(self.clients) == 1 and client_socket == sender_socket:
+                message = ('no one but you is active').encode('utf-8')
+                message_header = self.generate_header(message)
+                sender_socket.send(message_header + message)
 
-    print(f'Closed connection from {disconnected_user}')
-    
-    sockets_list.remove(disconnected_socket)
-    del clients[disconnected_socket]
-                
-    # notify the rest of the users for the leaving of user
-    leave_message = f'{disconnected_user} has left the chat!'.encode('utf-8')
-    leave_message_header = generate_header(leave_message)
+    def send_private_message(self, sender_user, receiver_user, message):
+        sender_user_encoded = sender_user.encode('utf-8')
+        receiver_user_encoded = receiver_user.encode('utf-8')
+        found = False
+        for client_socket in self.clients:
+            if self.clients[client_socket]['data'] == receiver_user_encoded:
+                private_message = f'privately {sender_user} > {message}'.encode('utf-8')
+                private_message_header = self.generate_header(private_message)
+                client_socket.send(private_message_header + private_message) 
+                found = True
 
-    for client in clients:
-        client.send(leave_message_header + leave_message)
+            if self.clients[client_socket]['data'] == sender_user_encoded:
+                sender_socket = client_socket
 
-
-def show_active_users(sender_socket):
-    
-    for client_socket in clients:
-        if client_socket != sender_socket:
-            message = ('->' + clients[client_socket]['data'].decode('utf-8')).encode('utf-8')
-            message_header = generate_header(message)
+        if not found:
+            message = f'{receiver_user} is not online'.encode('utf-8')
+            message_header = self.generate_header(message)
             sender_socket.send(message_header + message)
-        elif len(clients) == 1 and client_socket == sender_socket:
-            message = ('no one but you is active').encode('utf-8')
-            message_header = generate_header(message)
-            sender_socket.send(message_header + message)
 
-def send_private_message(sender_user, receiver_user, message):
-    sender_user_encoded = sender_user.encode('utf-8')
-    receiver_user_encoded = receiver_user.encode('utf-8')
-    found = False
-    for client_socket in clients:
-        if clients[client_socket]['data'] == receiver_user_encoded:
-            private_message = f'privately {sender_user} > {message}'
-            private_message_header = generate_header(private_message)
-            private_message = private_message.encode('utf-8')
-            client_socket.send(private_message_header + private_message) 
-            found = True
-        
-        if clients[client_socket]['data'] == sender_user_encoded:
-            sender_socket = client_socket
-    
-    if not found:
-        message = f'{receiver_user} is not online'
-        message_header = generate_header(message)
-        message = message.encode('utf-8')
-        sender_socket.send(message_header + message)
+    # broadcast sent message
+    def broadcast_message_to_users(self, sender_socket, message):
+        user = self.clients[sender_socket]
 
-# broadcast sent message
-def broadcast_message_to_users(sender_socket, message):
-    user = clients[sender_socket]
+        username = user['data'].decode('utf-8')
+        message_data = message['data'].decode('utf-8')
 
-    username = user['data'].decode('utf-8')
-    message_data = message['data'].decode('utf-8')
+        if message_data == '!who':
+            self.show_active_users(sender_socket)
+            return
+        elif message_data.split(' ')[0] == '!msg':
+            _, receiver_user, private_message = message_data.split(' ', 2)
+            self.send_private_message(username, receiver_user, private_message)
+            return
 
-    if message_data == '!who':
-        show_active_users(sender_socket)
-        return
-    elif message_data.split(' ')[0] == '!msg':
-        _, receiver_user, private_message = message_data.split(' ', 2)
-        send_private_message(username, receiver_user, private_message)
-        return
+        print(f'Received message from {username}: {message_data}')
 
-    print(f'Received message from {username}: {message_data}')
+        broadcast_message = f'{username} > {message_data}'.encode('utf-8')
+        broadcast_message_header = self.generate_header(broadcast_message)
 
-    broadcast_message = f'{username} > {message_data}'.encode('utf-8')
-    broadcast_message_header = generate_header(broadcast_message)
+        self.send_message_to_all_except_sender(sender_socket, broadcast_message_header + broadcast_message)
 
-    send_message_to_all_except_sender(sender_socket, broadcast_message_header + broadcast_message)
+    def run(self):
+        while True:
+            # select takes read list, write list and sockets we might error on 
+            read_sockets, _, exception_sockets = select.select(self.sockets_list, [], self.sockets_list)
 
+            for notified_socket in read_sockets:
+                # new connection
+                if notified_socket == self.server_socket: 
+                    client_socket, client_address = self.server_socket.accept()
 
-while True:
-    # select takes read list, write list and sockets we might error on 
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+                    user = self.receive_message(client_socket)
 
-    for notified_socket in read_sockets:
-        # new connection
-        if notified_socket == server_socket: 
-            client_socket, client_address = server_socket.accept()
+                    if user is False: 
+                        continue
 
-            user = receive_message(client_socket)
+                    self.handle_new_connection(client_socket, client_address,user)
+                # standard message
+                else:
+                    message = self.receive_message(notified_socket)
 
-            if user is False: 
-                continue
+                    # lost connection
+                    if message is False:
+                        self.handle_disconnection(notified_socket)
+                        continue
+                    
+                    self.broadcast_message_to_users(notified_socket, message)
 
-            handle_new_connection(client_socket, client_address,user)
-        # standard message
-        else:
-            message = receive_message(notified_socket)
+            for notified_socket in exception_sockets:
+                self.sockets_list.remove(notified_socket)
+                del self.clients[notified_socket]
 
-            # lost connection
-            if message is False:
-                handle_disconnection(notified_socket)
-                continue
-            
-            broadcast_message_to_users(notified_socket, message)
-    
-    for notified_socket in exception_sockets:
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
+if __name__ == '__main__':
+    server = ChatServer('127.0.0.1', 1234)
+    server.run()
